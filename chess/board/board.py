4b02c840782e72197b4ae5e5568c21f1e2984246
romualdas <romualdas.gircys@interstartas.lt>
2024-03-23 21:28:08 +0200

    add pawn promotion

--- chess/board/board.py
+++ chess/board/board.py
@@ -24,6 +24,10 @@ class Board:
         final = move.final
         self.squares[initial.row][initial.col].piece = None
         self.squares[final.row][final.col].piece = piece
+        
+        if isinstance(piece, Pawn):
+            self.check_promotion(piece, final)
+        
         piece.moved = True 
         piece.clear_moves()
         self.last_move = move
@@ -31,6 +35,11 @@ class Board:
     
     def valid_move(self, piece, move):
         return move in piece.moves
+    
+    
+    def  check_promotion(self, piece, final):
+        if final.row == 0 or final.row == 7:
+            self.squares[final.row][final.col].piece = Queen(piece.color)
         
 
     def calc_moves(self, piece, row, col):
