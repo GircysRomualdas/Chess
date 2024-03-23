9c6c74deb453d1b7e1a912da64fb89b8a6296a2f
romualdas <romualdas.gircys@interstartas.lt>
2024-03-23 11:03:57 +0200

    add knight_moves

--- chess/board/board.py
+++ chess/board/board.py
@@ -4,6 +4,7 @@ from chess.pieces.knight import Knight
 from chess.pieces.pawn import Pawn
 from chess.pieces.queen import Queen
 from chess.pieces.rook import Rook
+from chess.move import Move
 from config.constants import COLS, ROWS
 
 from .square import Square
@@ -13,34 +14,70 @@ class Board:
     def __init__(self):
         self.squares = [[0 for _ in range(COLS)] for _ in range(ROWS)]
         self._create()
-        self._add_pieces('white')
-        self._add_pieces('black')
+        self._add_pieces("white")
+        self._add_pieces("black")
 
+    def calc_moves(self, piece, row, col):
+
+        def knight_moves():
+            possible_moves = [
+                (row - 2, col + 1),
+                (row - 1, col + 2),
+                (row + 1, col + 2),
+                (row + 2, col + 1),
+                (row + 2, col - 1),
+                (row + 1, col - 2),
+                (row + 1, col - 2),
+                (row - 2, col - 1),
+            ]
+
+            for possible_move in possible_moves:
+                possible_move_row, possible_move_col = possible_move
+
+                if Square.in_range(possible_move_row, possible_move_col):
+                    if self.squares[possible_move_row][possible_move_col].is_empty_or_rival(piece.color):
+                        initial = Square(row, col)
+                        final = Square(possible_move_row, possible_move_col)
+                        
+                        move = Move(initial, final)
+                        piece.add_move(move)
+
+        if isinstance(piece, Pawn):
+            pass
+        elif isinstance(piece, Knight):
+            knight_moves()
+        elif isinstance(piece, Bishop):
+            pass
+        elif isinstance(piece, Rook):
+            pass
+        elif isinstance(piece, Queen):
+            pass
+        elif isinstance(piece, King):
+            pass
 
     def _create(self):
         for row in range(ROWS):
             for col in range(COLS):
                 self.squares[row][col] = Square(row, col)
 
-
     def _add_pieces(self, color):
-        if color == 'white':
+        if color == "white":
             row_pawn, row_other = (6, 7)
         else:
             row_pawn, row_other = (1, 0)
 
         for col in range(COLS):
-            self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(color) )
+            self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(color))
 
-        self.squares[row_other][1] = Square(row_other, 1, Knight(color) )
-        self.squares[row_other][6] = Square(row_other, 6, Knight(color) )
+        self.squares[row_other][1] = Square(row_other, 1, Knight(color))
+        self.squares[row_other][6] = Square(row_other, 6, Knight(color))
 
-        self.squares[row_other][2] = Square(row_other, 2, Bishop(color) )
-        self.squares[row_other][5] = Square(row_other, 5, Bishop(color) )
+        self.squares[row_other][2] = Square(row_other, 2, Bishop(color))
+        self.squares[row_other][5] = Square(row_other, 5, Bishop(color))
 
-        self.squares[row_other][0] = Square(row_other, 0, Rook(color) )
-        self.squares[row_other][7] = Square(row_other, 7, Rook(color) )
+        self.squares[row_other][0] = Square(row_other, 0, Rook(color))
+        self.squares[row_other][7] = Square(row_other, 7, Rook(color))
 
-        self.squares[row_other][3] = Square(row_other, 3, Queen(color) )
+        self.squares[row_other][3] = Square(row_other, 3, Queen(color))
 
-        self.squares[row_other][4] = Square(row_other, 4, King(color) )
+        self.squares[row_other][4] = Square(row_other, 4, King(color))
